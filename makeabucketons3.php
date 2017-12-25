fb12bafda319a3e011555f5af64f64c0e746a349
Micah Osamudiamen <osaosemwen@gmail.com>
2017-12-25 02:13:05 -0500

    Add files via upload

--- makeabucketons3.php
+++ makeabucketons3.php
@@ -15,10 +15,10 @@ $result = $client->createBucket(array(
 ));
 
 //HTML to Create our webpage
-echo "<h1 align=\"center\">HI Cloud Developers!</h1>";
+echo "<h1 align=\"center\">Hi Cloud Developers!</h1>";
 echo "<div align = \"center\"><img src=\"https://s3-us-west-1.amazonaws.com/mikesimpletutorial/images/cloud-network-map.jpg\"></img></div>";
-echo "<h2 align=\"center\">You have successfully created a bucket called {$bucket} you can check and confirm this!!</h2>";
-echo "<div align=\"center\"><a href=\"makefile.php?bucket=$bucket&key=$key\"> You can click Here to Continue</a></div>";
+echo "<h2 align=\"center\">You have successfully created a bucket called {$bucket} <br>you can check and confirm this!!</h2>";
+echo "<div align=\"center\"><a href=\"makefile.php?bucket=$bucket\"> You can click Here to Continue</a></div>";
 ?>
 
 
